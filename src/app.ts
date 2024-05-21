49b77c87ae9956ff79d69d899246cde885a506b4
sattamap <sattamap@gmail.com>
2024-05-22 01:52:19 +0600

    Add Prettier scripts for code formatting

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,8 @@
 import express, { Application, Request, Response } from "express";
 const app: Application = express();
 
-
 app.get("/", (req: Request, res: Response) => {
-    res.send("Welcome to express app!");
-  });
-
+  res.send("Welcome to express app!");
+});
 
 export default app;
